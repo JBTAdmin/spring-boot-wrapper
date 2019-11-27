cfaa419b5eb2733e1f58ed3529a466ec618229f9
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2019-11-27 21:06:43 +0530

    Initial Commit

---
+++ microservice-application/src/main/java/com/jbt/microserviceapplication/service/PersonServiceImpl.java
@@ -0,0 +1,52 @@
+package com.jbt.microserviceapplication.service;
+
+import com.jbt.microserviceapplication.model.Person;
+import com.jbt.microserviceapplication.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Override
+    public Person createPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    @Override
+    public Optional<Person> getPerson(Long id) {
+        return personRepository.findById(id);
+    }
+
+    @Override
+    public Person editPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    @Override
+    public void deletePerson(Person person) {
+        personRepository.delete(person);
+    }
+
+    @Override
+    public void deletePerson(Long id) {
+        personRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Person> getAllPersons(int pageNumber, int pageSize) {
+        return personRepository.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
+    }
+
+    @Override
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+}
