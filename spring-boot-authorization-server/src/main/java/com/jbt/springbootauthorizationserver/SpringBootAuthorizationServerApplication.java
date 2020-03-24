6e83b421b48d7f014e14c8aa324b82d68b21d42e
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-03-24 17:47:18 +0530

    Spring Security Authorization Server with Spring Boot

---
+++ spring-boot-authorization-server/src/main/java/com/jbt/springbootauthorizationserver/SpringBootAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.jbt.springbootauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthorizationServerApplication.class, args);
+	}
+
+}
