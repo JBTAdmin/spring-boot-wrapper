668aa75085ee33d01a0b122638ec67c00a4fa51f
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2021-10-30 09:22:26 +0530

    VERSION UPGRADE

--- spring-boot-mongodb/src/main/java/com/jbt/springbootmongodb/model/Contact.java
+++ spring-boot-mongodb/src/main/java/com/jbt/springbootmongodb/model/Contact.java
@@ -1,9 +1,6 @@
 package com.jbt.springbootmongodb.model;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-
 import java.io.Serializable;
 
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record Contact(String address, String phone) implements Serializable {
 }
